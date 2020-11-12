5468998de1f59d5510982f5241c60eb3ffd78e76
Caetano <98.ricardo.caetano@gmail.com>
2020-11-12 14:43:36 +0000

    tested quartos

--- proj2/run.sh
+++ proj2/run.sh
@@ -7,7 +7,7 @@ for i in sources/*.txt; do
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
 
-for i in tests/minutos_test/*.txt; do
+for i in tests/quartos_test/*.txt; do
 	echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled_test/$(basename $i ".txt").fst
 done
@@ -22,5 +22,5 @@ done
 
 for i in compiled_test/*.fst; do
 	echo "Testing the transducer compiled_test/$(basename $i '.fst')"
-	fstcompose compiled_test/$(basename $i) compiled/minutos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+	fstcompose compiled_test/$(basename $i) compiled/quartos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 done
