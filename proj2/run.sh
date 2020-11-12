8edeb2e098850127271d085797ca1eb9b4497041
Caetano <98.ricardo.caetano@gmail.com>
2020-11-12 00:36:31 +0000

    merge

--- proj2/run.sh
+++ proj2/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-mkdir -p compiled images
+mkdir -p compiled images compiled_test
 
-for i in sources/*.txt tests/*.txt; do
+for i in sources/*.txt; do
 	echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
 
+for i in tests/horas_test/*.txt; do
+	echo "Compiling: $i"
+    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled_test/$(basename $i ".txt").fst
+done
 
 # TODO 
 
@@ -16,5 +20,7 @@ for i in compiled/*.fst; do
     fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
 done
 
-echo "Testing the transducer"
-fstcompose compiled/t22.fst compiled/horas.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+for i in compiled_test/*.fst; do
+	echo "Testing the transducer compiled_test/$(basename $i '.fst')"
+	fstcompose compiled_test/$(basename $i) compiled/horas.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+done
