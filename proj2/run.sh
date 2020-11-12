cdaccd6bac3723d436b01f25bb143acf25cd26b0
Caetano <98.ricardo.caetano@gmail.com>
2020-11-12 17:02:32 +0000

    num2text, found problem 20

--- proj2/run.sh
+++ proj2/run.sh
@@ -7,12 +7,18 @@ for i in sources/*.txt; do
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
 
-for i in tests/quartos_test/*.txt; do
+for i in tests/text2num_test/*.txt; do
 	echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled_test/$(basename $i ".txt").fst
 done
 
 # TODO 
+
+# text2num FIXME: exemplo vinte e quinze minutos | vinte e quarenta e cinco minutos etc ... problema no vinte ele le o "e" e espera uma duas tres
+
+fstconcat compiled/horas.fst compiled/e.fst > compiled/text2num_aux.fst
+fstconcat compiled/text2num_aux.fst compiled/minutos.fst > compiled/text2num.fst
+rm compiled/text2num_aux.fst
 
 
 for i in compiled/*.fst; do
@@ -22,5 +28,5 @@ done
 
 for i in compiled_test/*.fst; do
 	echo "Testing the transducer compiled_test/$(basename $i '.fst')"
-	fstcompose compiled_test/$(basename $i) compiled/quartos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+	fstcompose compiled_test/$(basename $i) compiled/text2num.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 done
