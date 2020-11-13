d6f08c17e49cb7d84002aff09f6102d473301b27
BernardoFaria <bernardo.faria@tecnico.ulisboa.pt>
2020-11-13 19:37:24 +0000

    rich2num done

--- proj2/run.sh
+++ proj2/run.sh
@@ -7,7 +7,7 @@ for i in sources/*.txt; do
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
 
-for i in tests/num2text_test/*.txt; do
+for i in tests/rich2num_test/*.txt; do
 	echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled_test/$(basename $i ".txt").fst
 done
@@ -31,6 +31,16 @@ fstunion compiled/quartos.fst compiled/meias.fst > compiled/rich2text_aux2.fst
 fstconcat compiled/rich2text_aux.fst compiled/rich2text_aux2.fst > compiled/rich2text.fst
 rm compiled/rich2text_aux.fst compiled/rich2text_aux2.fst
 
+# rich2num
+fstcompose compiled/meias.fst compiled/minutos.fst > compiled/rich2num_aux1.fst # talvez um project_type=output
+fstcompose compiled/quartos.fst compiled/minutos.fst > compiled/rich2num_aux2.fst
+fstunion compiled/rich2num_aux1.fst compiled/rich2num_aux2.fst > compiled/rich2num_aux3.fst
+fstunion compiled/minutos.fst compiled/rich2num_aux3.fst > compiled/rich2num_aux4.fst
+fstconcat compiled/e.fst compiled/rich2num_aux4.fst > compiled/rich2num_aux5.fst
+fstunion compiled/rich.fst compiled/rich2num_aux5.fst > compiled/rich2num_aux6.fst
+fstconcat compiled/horas.fst compiled/rich2num_aux6.fst > compiled/rich2num.fst 
+rm compiled/rich2num_aux1.fst compiled/rich2num_aux2.fst compiled/rich2num_aux3.fst compiled/rich2num_aux4.fst compiled/rich2num_aux5.fst compiled/rich2num_aux6.fst
+
 # num2text FIXME: exemplo vinte e quinze minutos | vinte e quarenta e cinco minutos etc ... problema no vinte ele le o "e" e espera uma duas tres
 fstinvert compiled/horas.fst > compiled/invert_horas.fst 
 fstinvert compiled/minutos.fst > compiled/invert_minutos.fst 
@@ -46,7 +56,7 @@ done
 
 for i in compiled_test/*.fst; do
 	echo "Testing the transducer compiled_test/$(basename $i '.fst')"
-	fstcompose compiled_test/$(basename $i) compiled/num2text.fst  | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+	fstcompose compiled_test/$(basename $i) compiled/rich2num.fst  | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 done
 
 rm compiled/* compiled_test/*
